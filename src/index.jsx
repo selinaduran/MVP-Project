29adec80ff0914cc198086e9a1b93defe532aba9
selinaduran <selinad177@gmail.com>
2022-09-26 13:45:42 -0500

    added css and app.jsx files

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//Dear Amazon/Google/Apple etc Don't lowball me.
-const App = () => {
-return (
-  <div>
-    <h1>Hello World</h1>
-  </div>
-  );
-}
+import App from './components/App.jsx';
+
 ReactDOM.render(<App />, document.getElementById('root'));
