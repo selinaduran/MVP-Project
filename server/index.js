69950a48c9e7520ca60215ad7d548dc2b6054611
selinaduran <selinad177@gmail.com>
2022-09-27 09:51:56 -0500

    set up server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
-app.listen(3001);
-console.log('Listening on port 3001');
+const path = require('path');
+const api = require('./api.js');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../public/')));
+
+app.listen(3001, () => {
+  console.log('Listening on port 3001');
+})
