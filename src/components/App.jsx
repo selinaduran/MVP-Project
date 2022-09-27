317e1f44cbd9223531a5f596909d771ca4a2e9ae
selinaduran <selinad177@gmail.com>
2022-09-27 16:43:13 -0500

    added choice selectors for cards

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,8 @@ const App = () => {
   const [villagers, setVillagers] = useState([])
   const [cards, setCards] = useState([])
   const [counter, setCounter] = useState(0)
+  const [selectOne, setSelectOne] = useState(null)
+  const [selectTwo, setSelectTwo] = useState(null)
 
   useEffect(() => {
     axios.get('https://acnhapi.com/v1/villagers')
@@ -30,17 +32,43 @@ const App = () => {
     return shuffled;
   }
 
+  const makeNewGame = (event) => {
+    window.location.reload()
+  }
+
+  const handleSelect = (card) => {
+    selectOne ? setSelectTwo(card) : setSelectOne(card)
+  }
+
+  useEffect(() => {
+    if (selectOne && selectTwo) {
+      if (selectOne.image_uri === selectTwo.image_uri) {
+        console.log('You got a match!');
+        resetTurn()
+      } else {
+        console.log('Sorry, try again!')
+        resetTurn()
+      }
+    }
+  }, [selectOne, selectTwo])
+
+  const resetTurn = () => {
+    setSelectOne(null)
+    setSelectTwo(null)
+    setCounter(counter + 1)
+  }
+
   return (
     <div className="app">
       <h1 className="game-title">Animal Crossing Memory Match!</h1>
       <div className="game-board">
         {cards.map(card => (
-          <Card key={card.id} card={card}/>
+          <Card key={card.id} card={card} handleSelect={handleSelect}/>
         ))}
       </div>
       <div>
         <button>Submit Score</button>
-        <button>New Game</button>
+        <button onClick={makeNewGame}>New Game</button>
       </div>
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
-const Card = (props) => {
+const Card = ({ card, handleSelect }) => {
+  const handleClick = () => {
+    handleSelect(card)
+  }
+
   return (
     <div className="card">
       <div>
-        <img className="front" src={props.card.image_uri} alt="card front" />
-        <img className="back" src="/img/cover2.jpeg" alt="card back" />
+        <img className="front" src={card.image_uri} alt="card front" />
+        <img className="back" src="/img/cover2.jpeg" onClick={handleClick} alt="card back" />
       </div>
     </div>
   )
