42406655a43c7a61cb1c1b06042adfc34034271e
selinaduran <selinad177@gmail.com>
2022-09-27 09:26:17 -0500

    added game title and buttons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="app">
+      <h1 className="game-title">Animal Crossing Memory Match!</h1>
+      <div>
+        <button>Submit Score</button>
+        <button>New Game</button>
+      </div>
     </div>
   );
 }
